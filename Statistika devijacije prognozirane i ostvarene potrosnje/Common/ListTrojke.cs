e4a235354e6b0b927783313a3a1d44cc13795741
vukbajic <39351093+vukbajic@users.noreply.github.com>
2018-06-01 15:16:41 +0200

    XML Baza podataka
    Odradjeno sve, ima mali bag kod Serijalizacije. Treba da se resi veceras

---
+++ Statistika devijacije prognozirane i ostvarene potrosnje/Common/ListTrojke.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Common
+{
+    [XmlRoot("TROJKE_LOAD")]
+    public class ListTrojke
+    {
+        
+        private List<Trojke> trojke;
+        [XmlElement("Trojke")]
+        public List<Trojke> Trojke
+        {
+            get { return trojke; }
+            set { trojke = value; }
+        }
+
+        public ListTrojke()
+        {
+            trojke = new List<Trojke>();
+        }
+    }
+}
